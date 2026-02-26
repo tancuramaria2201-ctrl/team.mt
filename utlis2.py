f50e4faeb984596887fcabce59b8e4e0db37b163
Morozova Maria <morozova.maria@knu.ua>
2026-02-26 20:45:08 +0200

    Додано функцію is_prime для перевірки простих чисел

--- utlis2.py
+++ utlis2.py
@@ -8,3 +8,11 @@ def factorial(n):
     for i in range(2, n + 1):
         result *= i
     return result
+ def is_prime(n):
+       """Перевіряє, чи є число n простим."""
+       if not isinstance(n, int) or n < 2:
+           return False
+       for i in range(2, int(n**0.5) + 1):
+           if n % i == 0:
+               return False
+       return True
