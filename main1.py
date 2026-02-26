029a6130bcd0d91d05d837a310b6041d6db89369
Morozova Maria <morozova.maria@knu.ua>
2026-02-26 21:10:02 +0200

    Додано виклик функції is_prime в main1.py

--- main1.py
+++ main1.py
@@ -4,4 +4,9 @@ if name == "main":
     number = 5
     fact = utils.factorial(number)
     print(f"Факторіал числа {number} дорівнює {fact}")
+    test_number = 7
+    if utils.is_prime(test_number):
+        print(f"Число {test_number} є простим")
+    else:
+        print(f"Число {test_number} не є простим")
     
