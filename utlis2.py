020f3c274c4ca2529b12ae4f52a8eb4c750d9a26
tancuramaria2201-ctrl <tancuramaria2201@gmail.com>
2026-02-27 15:30:09 +0200

    додна функцію is_prime для перевірки простих чисел

--- utlis2.py
+++ utlis2.py
@@ -16,3 +16,16 @@ def factorial(n):
            if n % i == 0:
                return False
        return True
+def gcd(a, b):
+    
+    while b:
+        a, b = b, a % b
+    return a
+def is_prime(n):
+   
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
